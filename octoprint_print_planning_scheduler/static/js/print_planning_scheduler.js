41f9bb74dd9e74d08e1b15820662a4335eaf0440
Anton Skrypnyk <antons@moduleworks.com>
2024-07-30 18:35:16 +0300

    WIP Add ui bindings for plugin API

--- octoprint_print_planning_scheduler/static/js/print_planning_scheduler.js
+++ octoprint_print_planning_scheduler/static/js/print_planning_scheduler.js
@@ -9,42 +9,77 @@ class print_planning_schedulerViewModel
     constructor(parameters)
     {
         this.calendar = null;
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
-
-        // TODO: Implement your plugin's view model here.
+        this.COLOR_DISABLED_PRINTING = "#c92662";
+        this.COLOR_ENABLED_PRINTING = "#03fc73";
+        this.PLUGIN_BASE_URL = "/plugin/print_planning_scheduler";
     }
 
     onStartupComplete()
     {
         this.initCalendar();
-        this.initNewEventForm();
+        this.initCalendarControlForms();
     }
 
     initCalendar()
     {
-        var calendarEl = document.getElementById('scheduling_calendar');
-        this.calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: "timeGridWeek",
-            initialDate: new Date().toISOString().split("T", 1)[1],
-            headerToolbar: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'timeGridWeek,timeGridDay'
-            },
-            nowIndicator: true,
-            navLinks: true,
-            selectable: true,
-            selectMirror: true,
-            select: this.addEventByPrompt.bind(this),
-            eventClick: this.onClickEvent.bind(this),
-            editable: true,
-            dayMaxEvents: true,
-        });
+        $(document).ready(() => {
+            const calendarEl = $("#scheduling_calendar").get(0);
+            this.calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: "timeGridWeek",
+                initialDate: new Date().toISOString().split("T", 1)[1],
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'timeGridWeek,timeGridDay'
+                },
+                nowIndicator: true,
+                navLinks: true,
+                selectable: true,
+                selectMirror: true,
+                select: this.addEventByPrompt.bind(this),
+                eventClick: this.onClickEvent.bind(this),
+                editable: true,
+                dayMaxEvents: true,
+                eventSources: [
+                    {
+                        events: this.onCalendarEventsUpdate.bind(this),
+                        color: self.COLOR_DISABLED_PRINTING,
+                        textColor: 'black'
+                    }
+                ],
+            });
+            this.calendar.render();
 
-        this.calendar.changeView("timeGridWeek");
-        this.calendar.render();
+            // Hack around issue https://github.com/fullcalendar/fullcalendar/issues/4976
+            window.setTimeout(() => {
+                window.dispatchEvent(new Event("resize"));
+            }, 100);
+        });
+    }
+
+    onCalendarEventsUpdate(info, successCallback, failureCallback)
+    {
+        $.ajax({
+            url: this.PLUGIN_BASE_URL + '/disabled_event',
+            type: 'GET',
+            data: {
+                start: info.start.toISOString(),
+                end: info.end.toISOString(),
+            },
+            success: function(response) {
+                successCallback(
+                    response.slice().map((event) => {
+                            return {
+                                title: event.name,
+                                start: new Date(event.start).toISOString(),
+                                end: new Date(event.end).toISOString(),
+                            }
+                        }));
+            },
+            error: function(xhr, status, error) {
+                failureCallback(error);
+            }
+        });
     }
 
     addEventByPrompt(newEventProps)
@@ -68,24 +103,49 @@ class print_planning_schedulerViewModel
         }
     }
 
-    initNewEventForm()
+    initCalendarControlForms()
     {
-        const form = $('#scheduling_calendar_event_form');
-        $('#scheduling_calendar_event_submit').on('click', this.addFormEvent.bind(this, form));
+        $('#schedule_upload_form').on('submit', e => this.onScheduleFileUploadSubmit(e));
+        $('#scheduling_calendar_event_form').on('submit', e => this.onAddCalendarEventSubmit(e));
+        $('#print_job_form').on('submit', e => this.onAddPrintJobSubmit(e));
     }
 
-    addFormEvent(form)
+    onScheduleFileUploadSubmit(event) {
+        event.preventDefault();
+        const fileEl = $(event.target).get(0);
+        const formData = new FormData(fileEl);
+        console.log(...formData);
+
+        $.ajax({
+            url: this.PLUGIN_BASE_URL + '/upload_schedule',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                $('#message').text(response.message).css('color', 'green');
+            },
+            error: function (xhr, status, error) {
+                var errorMessage = xhr.responseJSON ? xhr.responseJSON.message : 'An error occurred';
+                $('#message').text(errorMessage).css('color', 'red');
+            }
+        });
+    }
+
+    onAddCalendarEventSubmit(event)
     {
+        event.preventDefault();
+        const formData = new FormData($(event.target).get(0));
+
         var newEvent = {
-            title: form.find('[name="title"]').val(),
-            url: form.find('[name="url"]').val(),
-            start: form.find('[name="start"]').val(),
-            end: form.find('[name="end"]').val()
+            title: formData.title,
+            start: new Date(formData.start).toISOString(),
+            end: new Date(formData.end).toISOString()
         };
 
         if (newEvent.start == "")
         {
-            newEvent.start = new Date().toISOString();
+            newEvent.start = new Date(Date.now()).toISOString();
         }
         if (newEvent.end == "")
         {
@@ -93,18 +153,69 @@ class print_planning_schedulerViewModel
             newEnd.setMinutes(newEnd.getMinutes() + 30);
             newEvent.end = newEnd.toISOString();
         }
+        
+        var isPrintingEnabled = form.find('[name="isPrintingEnabled"]').val();
+        newEvent.eventColor = isPrintingEnabled ? COLOR_ENABLED_PRINTING : COLOR_DISABLED_PRINTING;
 
-        this.addCalendarEvent(newEvent);
+        if (isPrintingEnabled)
+        {
+            this.sendDisabledEvent({
+                name: newEvent.title,
+                start: newEvent.start,
+                end: newEvent.end,
+            },
+            () => this.calendar.addEvent(newEvent));
+        } else {
+            this.sendExcludedInterval({
+                start: newEvent.start,
+                end: newEvent.end,
+            },
+            () => this.calendar.addEvent(newEvent));
+        }
     }
 
-    addCalendarEvent(event) {
-        if (this.calendar == null)
-        {
-            console.log("Cannot add event for non existing calendar")
-            return
-        }
-        this.calendar.addEvent(event);
-        console.info(event);
+    onAddPrintJobSubmit(event)
+    {
+        submit_event.preventDefault();
+        const formData = new FormData($(event.target).get(0));
+
+        const printJob = {
+            name: formData.title,
+            duration: formData.duration,
+            description: formData.description
+        };
+
+        console.log('Print Job:', printJob);
+        // TODO: send job
+    }
+
+    sendDisabledEvent(newEvent) {
+        $.ajax({
+            url: this.PLUGIN_BASE_URL + '/disabled_event',
+            type: 'POST',
+            data: newEvent,
+            success: function (response) {
+                console.log('Disabled event post:', response);
+            },
+            error: function (xhr, status, error) {
+                console.error('Error fetching disabled events:', error);
+            }
+        });
+    }
+
+    sendExcludedInterval(excludedInterval, success=null)
+    {
+        $.ajax({
+            url: this.PLUGIN_BASE_URL + '/excluded_interval',
+            type: 'POST',
+            data: excludedInterval,
+            success: success != null ? success : function(response) {
+                console.log('Interval excluded successfully:', response);
+            },
+            error: function(xhr, status, error) {
+                console.error('Error excluding interval:', error);
+            }
+        });
     }
 }
 
